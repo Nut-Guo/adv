6c45f0f094b1b94b4927fe41bab2624d98205960
Nut-Guo <292998520@qq.com>
2021-05-18 13:51:23 +0800

    update settings

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -87,7 +87,6 @@ class PatchNet(pl.LightningModule):
 
     def validation_step(self, batch: Any, batch_idx: int) -> torch.Tensor:
         loss = self.step(batch, batch_idx)
-        self.log("val_loss", loss)
         self.log_dict(
             {"val_loss": loss},
             on_step=False,
