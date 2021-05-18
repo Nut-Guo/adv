08b6469f2537baa87979224972266f901c005064
Nut-Guo <292998520@qq.com>
2021-05-18 17:57:36 +0800

    log patched_image

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -70,8 +70,8 @@ class PatchNet(pl.LightningModule):
         self.log('det_loss', det_loss)
         self.log('tv_loss', tv_loss)
         trans = transforms.ToTensor()
-        self.log('patched_img', wandb.Image(image_batch[0].clone().detach()), on_step=True,
-                 reduce_fx=lambda x: wandb.Image(torchvision.utils.make_grid([trans(i) for i in x])))
+        self.log('patched_img', Image(image_batch[0].clone().detach()), on_step=True,
+                 reduce_fx=lambda x: x[-1])
         loss = det_loss + torch.max(tv_loss, torch.tensor(0.1))
         return loss
 
@@ -79,7 +79,7 @@ class PatchNet(pl.LightningModule):
         loss = self.step(batch, batch_idx)
         patch = wandb.Image(self.patch.clone().detach())
         trans = transforms.ToTensor()
-        self.log('patch', patch, on_step=True, reduce_fx=lambda x: wandb.Image(torchvision.utils.make_grid([trans(i) for i in x])))
+        self.log('patch', patch, on_step=True, reduce_fx=lambda x: x[-1])
         self.log_dict(
             {
                 "train_loss": loss,
