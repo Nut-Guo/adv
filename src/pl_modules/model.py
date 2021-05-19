f755f77d3595afa86f3ba9ffaf6bbccf9452e361
Nut-Guo <292998520@qq.com>
2021-05-19 11:13:23 +0800

    debug

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -71,7 +71,7 @@ class PatchNet(pl.LightningModule):
         pred = self.pred_extractor(detections)
         tv = self.total_variation(self.patch)
         tv_loss = tv * 0.1
-        det_loss = -torch.log(1 - torch.mean(torch.cat(pred))) if len(pred) > 0 else torch.tensor(0.1)
+        det_loss = -torch.sum(torch.log(1 - (torch.cat(pred)))) if len(pred) > 0 else torch.tensor(0.1)
         self.log_dict(
             {
                 'det_loss': det_loss,
