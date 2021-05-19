c5bb17cae24b1ea650960142e1f0a3a061583690
Nut-Guo <292998520@qq.com>
2021-05-19 19:33:17 +0800

    apply patch in the middle

--- src/pl_modules/patch.py
+++ src/pl_modules/patch.py
@@ -78,9 +78,9 @@ class PatchTransformer(nn.Module):
         return result
 
     def placeinto_box(self, patch, box, base):
-        print(self.image_size)
         box[2] = min(self.image_size, box[2])
         box[3] = min(self.image_size, box[3])
+        print(box)
         size = int(min(box[2] - box[0], box[3]-box[1]).item() // 2) * 2 * self.portion
         trans = transforms.Resize((size, size))
         patch = trans(patch)
