210cd5eb8bb71b5cf855769eab4ba5b575904b29
Nut-Guo <292998520@qq.com>
2021-05-19 20:36:32 +0800

    update patch

--- src/pl_modules/patch.py
+++ src/pl_modules/patch.py
@@ -81,7 +81,7 @@ class PatchTransformer(nn.Module):
         # box[2] = min(self.image_size, box[2])
         # box[3] = min(self.image_size, box[3])
         box = box.clamp(0, self.image_size)
-        box = [max(min(self.image_size, self.image_size - int(p)), 0) for p in box]
+        box = [max(min(self.image_size, int(p)), 0) for p in box]
         size = int((min(box[2] - box[0], box[3]-box[1]) // 2) * 2 * self.portion)
         trans = transforms.Resize((size, size))
         patch = trans(patch)
