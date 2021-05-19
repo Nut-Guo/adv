3cf74d05518a3d82fa6e47ad86847fba743f2cc5
Nut-Guo <292998520@qq.com>
2021-05-19 21:23:25 +0800

    update patch

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -78,7 +78,7 @@ class PatchNet(pl.LightningModule):
                 'patch': wandb.Image(self.patch.clone().detach()),
                 'adv_patch': wandb.Image(adv_batch[0].clone().detach()),
                 'patched_img': wandb.Image(
-                    torchvision.utils.draw_bounding_boxes(((image_batch[0] * 255).clone().detach().cpu()).byte(),
+                    torchvision.utils.draw_bounding_boxes((image_batch[0] * 255).clone().detach().cpu().byte(),
                                                           gt_output['boxes'][0],
                                                           width=4))
             })
