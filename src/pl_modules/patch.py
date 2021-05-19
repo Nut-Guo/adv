718b80c2a38ce001486d7bc11e7abdaa2b18bd9c
Nut-Guo <292998520@qq.com>
2021-05-19 19:55:56 +0800

    update patch

--- src/pl_modules/patch.py
+++ src/pl_modules/patch.py
@@ -90,10 +90,8 @@ class PatchTransformer(nn.Module):
         x2 = midx + size//2
         y1 = midy - size//2
         y2 = midy + size//2
-        try:
-            base[:, x1:x2, y1:y2] = patch
-        except:
-            print(x1, x2, y1, y2)
+        print(x1, x2, y1, y2)
+        base[:, x1:x2, y1:y2] = patch
         return base
 
     def forward(self, adv_patch, ground_truth):
