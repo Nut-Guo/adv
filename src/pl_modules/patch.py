f1031e5f2031507ca4c5e468d02fbea16d7119ed
Nut-Guo <292998520@qq.com>
2021-05-19 19:49:50 +0800

    update patch

--- src/pl_modules/patch.py
+++ src/pl_modules/patch.py
@@ -45,9 +45,9 @@ class PatchTransformer(nn.Module):
         self.portion = portion
         self.pad_size = (image_size - patch_size) // 2
         self.transforms = transforms.Compose([
-            transforms.Pad(
-                self.pad_size
-            ),
+            # transforms.Pad(
+            #     self.pad_size
+            # ),
             transforms.RandomAffine(
                 degrees=degrees,
                 translate=translate,
