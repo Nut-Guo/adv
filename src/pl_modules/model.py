c8f89dd055d62dcd207c387dc4ee39fa64b8259b
Nut-Guo <292998520@qq.com>
2021-05-20 13:50:56 +0800

    update settings

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -83,7 +83,8 @@ class PatchNet(pl.LightningModule):
             det_loss = torch.tensor(0.)
 
         trans = transforms.ToPILImage()
-        image = trans((image_batch[0].clone() * 255).detach())
+        image = trans(image_batch[0].clone()).detach()
+        image = torch.nn.functional.to_tensor(image)
         bbox_image = draw_bounding_boxes(image, pred['boxes'][0], width=3,
                                          font="LiberationMono-Bold.ttf", font_size=30)
         if batch_idx % self.log_interval == 0:
