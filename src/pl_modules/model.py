6d60410d2666fd1866faed45245573ea92b36c23
Nut-Guo <292998520@qq.com>
2021-05-20 14:34:06 +0800

    update settings

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -85,7 +85,7 @@ class PatchNet(pl.LightningModule):
         image = image_batch[0].clone().clamp(0, 1).detach().cpu()
         image = (image * 255).byte()
         boxes = pred['boxes'][0].clone().clamp(0, 1).detach().cpu().byte()
-        bbox_image = draw_bounding_boxes(image, boxes, width=3,
+        bbox_image = draw_bounding_boxes(image, boxes, width=10,
                                          font="LiberationMono-Bold.ttf", font_size=30)
         if batch_idx % self.log_interval == 0:
             self.logger.experiment.log({
