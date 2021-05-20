633edbfb78a5ab65928a8a3b871aeb0283cfdf7b
Nut-Guo <292998520@qq.com>
2021-05-20 13:40:29 +0800

    update settings

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -85,7 +85,8 @@ class PatchNet(pl.LightningModule):
             image = (image_batch[0].clone() * 255).detach().cpu().byte()
             bbox_image = draw_bounding_boxes(image, detections['boxes'][0], width=3,
                                              font="LiberationMono-Bold.ttf", font_size=30)
-        print(detections)
+        except:
+            print(detections)
         if batch_idx % self.log_interval == 0:
             self.logger.experiment.log({
                 'patch': wandb.Image(self.patch.clone().detach()),
