d67661c2d37126e3a07c64f567497c28fc42bad3
Nut_Guo <292998520@qq.com>
2021-05-24 14:06:00 +0800

    update patch_transforms config

--- src/pl_modules/patch.py
+++ src/pl_modules/patch.py
@@ -46,7 +46,7 @@ class PatchTransformer(nn.Module):
         # self.pad_size = (image_size - patch_size) // 2
         self.base = nn.Parameter(torch.zeros((3, self.image_size, self.image_size)))
         self.register_parameter(name='base', param=self.base)
-        self.transforms = patch_transforms
+        self.transforms = transforms.Compose(list(patch_transforms.values()))
         # self.transforms = transforms.Compose([
         #     # transforms.Pad(
         #     #     self.pad_size
