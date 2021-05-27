cc79be04d29732075e717ecdb1e1952335f03129
Nut-Guo <292998520@qq.com>
2021-05-27 11:29:18 +0800

    convert to albumentations

--- src/pl_data/dataset.py
+++ src/pl_data/dataset.py
@@ -13,6 +13,8 @@ from torch.utils.data import Dataset
 from PIL import Image
 from src.common.utils import PROJECT_ROOT
 import albumentations as A
+from albumentations.pytorch import ToTensor
+import cv2
 
 
 class DHDDataset(Dataset):
@@ -60,24 +62,34 @@ class PersonDataset(object):
         # self.imgs = list(sorted(os.listdir(self.path)))
         if max_size:
             self.imgs = self.imgs[:max_size]
-        self.transforms = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize((image_size, image_size)),
-        ])
+        # self.transforms = transforms.Compose([
+        #     transforms.ToTensor(),
+        #     transforms.Resize((image_size, image_size)),
+        # ])
+        self.transforms = A.Compose([
+            A.RandomBrightnessContrast(p=0.2),
+            A.HorizontalFlip(),
+            A.Resize(image_size, image_size),
+            ToTensor()
+        ], bbox_params=A.BboxParams(format='albumentations'))
 
     def __getitem__(self, idx):
         name = self.imgs[idx]
         img_path = os.path.join(self.path, name)
-        img = Image.open(img_path).convert("RGB")
-        boxes = torch.Tensor(self.anno[name]['boxes'])
-        boxes = boxes.clamp(0, 1) * self.image_size
-        confidence = torch.Tensor(self.anno[name]['confidence'])
-        if self.transforms is not None:
-            img = self.transforms(img)
+        # img = Image.open(img_path).convert("RGB")
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        boxes = self.anno[name]['boxes']
+        confidence = self.anno[name]['confidence']
+        # boxes = boxes.clamp(0, 1) * self.image_size
+        # confidence = torch.Tensor(self.anno[name]['confidence'])
+        # if self.transforms is not None:
+        #     img = self.transforms(img)
+        transformed = self.transforms(image=img, bboxes=boxes, probs=confidence)
         return {
-            "image": img,
-            "boxes": boxes,
-            "classprobs": confidence
+            "image": transformed["image"],
+            "boxes": transformed["bboxes"],
+            "classprobs": transformed["probs"]
         }
 
     def __len__(self):
