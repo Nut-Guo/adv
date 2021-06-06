bc3edd2ef72ba263c116b8034cc7f7843bd7389b
Nut_Guo <292998520@qq.com>
2021-06-06 19:13:22 +0800

    add copy paste augmentation

--- src/pl_data/coco.py
+++ src/pl_data/coco.py
@@ -73,8 +73,8 @@ class CocoDetectionCP(CocoDetection):
 
         # filter images without detection annotations
         ids = []
-        catIds = self.coco.getCatIds(catNms=filter_classes)
-        self.ids = self.coco.getImgIds(catIds=catIds)
+        # catIds = self.coco.getCatIds(catNms=filter_classes)
+        # self.ids = self.coco.getImgIds(catIds=catIds)
         for img_id in self.ids:
             ann_ids = self.coco.getAnnIds(imgIds=img_id, iscrowd=None)
             anno = self.coco.loadAnns(ann_ids)
